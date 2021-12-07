d524fee2dd0fad25d07b0305806caa2ef0a36619
GummyGuppy <alphastack.dev@gmail.com>
2021-12-06 19:41:05 -0600

    version 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This text is version 2 baby
         </a>
       </header>
     </div>
